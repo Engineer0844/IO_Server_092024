7590ddbd77c29972eb7bc7f0128b48bb8f11855a
Engineer0844 <git@github.com:Engineer0844>
2024-09-14 13:21:26 -0500

    instead of a label switched to a table

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,68 @@ import useWebSocket from 'react-use-websocket';
 
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const WS_URL = 'ws://' + window.location.host + '/ws';
 
+
+function IoStateRow({ label_name }) {
+
+  const [value, setValue] = useState(10);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+    },
+    share: true
+  });
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const json_payload = JSON.parse(lastMessage.data);
+      if(json_payload["id"] == label_name) {
+        setValue(json_payload["text"]);
+      }
+    }
+  }, [lastMessage]);
+
+  return (
+    <tr>
+      <td>{label_name}</td>
+      <td>{value}</td>
+    </tr>
+  );
+}
+
+function IoStateTable() {
+  
+  const pin_names = [
+    "adc1_channel0",
+    "adc1_channel1",
+    "adc1_channel2",
+    "adc1_channel3",
+    "adc2_channel0",
+    "adc2_channel1",
+    "adc2_channel2",
+    "adc2_channel3",
+    "pin_one",
+    "pin_two"
+  ];
+
+  const body = pin_names.map((x) => <IoStateRow label_name={x}/>);
+  return (<Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Pin Name</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {body}
+      </tbody>
+    </Table>);
+}
 
 // this is text update label
 function LabelDisplay({ label_name }) {
@@ -49,35 +105,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div>
-        <LabelDisplay label_name="adc1_channel0"/>
-        <LabelDisplay label_name="adc1_channel1"/>
-        <LabelDisplay label_name="adc1_channel2"/>
-        <LabelDisplay label_name="adc1_channel3"/>
-        <LabelDisplay label_name="adc2_channel0"/>
-        <LabelDisplay label_name="adc2_channel1"/>
-        <LabelDisplay label_name="adc2_channel2"/>
-        <LabelDisplay label_name="adc2_channel3"/>
-
-        <LabelDisplay label_name="pin_one"/>
-        <LabelDisplay label_name="pin_two"/>
-
-
-        <Button variant="primary">Primary</Button>
+        <IoStateTable />
       </div>
     </div>
   );
