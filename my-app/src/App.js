93979c1033481200ed83fefcf73286f8c67c630c
Engineer0844 <git@github.com:Engineer0844>
2024-09-14 12:15:47 -0500

    updated client to receive websocket data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,82 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useWebSocket from 'react-use-websocket';
 
 
 import logo from './logo.svg';
 import './App.css';
 
-function Square({ value, onSquareClick }) {
-    return (
-	    <button className="square" onClick={onSquareClick}>
-	    {value}
-	</button>
-    );
-}
-
-function Board({ xIsNext, squares, onPlay }) {
-    function handleClick(i) {
-	if(calculateWinner(squares) || squares[i]) {
-	    
-	}
-    }
-
-    let status = 'winner';
-
-
-    return (
-	    <>
-	    <div className="status">{status}</div>
-	    <div className="board-row">
-	      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-	      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-	      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-	    </div>
-	    </>
-    );
-}
-
-function calculateWinner(squares)  {
-    const lines = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]	
-    ];
-    for (let i = 0; i < lines.length; i++) {
-	const [a, b, c] = lines[i];
-	if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-	    return squares[a];
-	}
-    }
-    return null;
-}
-
 const WS_URL = 'ws://' + window.location.host + '/ws';
 
+
+
+function LabelDisplay() {
+  
+  const [value, setValue] = useState(10);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+    },
+    share: true
+  });
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const json_payload = JSON.parse(lastMessage.data);
+      if(json_payload["id"] == "adc-1") {
+        setValue(json_payload["text"]);
+      }
+    }
+  }, [lastMessage]);
+
+  return (
+    <div className="label">
+      <p>Label: </p> <p>{value}</p>
+    </div>
+  );
+}
 
 function App() {
 
     useWebSocket(WS_URL, {
-	onOpen: () => {
-	    console.log('WebSocket connection established.');
-	}
+	    onOpen: () => {
+	      console.log('WebSocket connection established.');
+	    },
+      share: true
     });
 
-
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
-
-
-    function handlePlay(nextSquares) {
-	const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    }
-    
   return (
     <div className="App">
       <header className="App-header">
@@ -93,8 +60,8 @@ function App() {
           Learn React
         </a>
       </header>
-      <div className="game">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <div>
+        <LabelDisplay />
       </div>
     </div>
   );
